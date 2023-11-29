80d8bdcfeb0672d2fdb5dabeb544309c05ad04fc
garatquent <garatquent@eisti.eu>
2023-11-29 15:39:02 +0100

    maps position GPS point on get

--- tracker-front/script.js
+++ tracker-front/script.js
@@ -1,19 +1,26 @@
 import 'regenerator-runtime/runtime';
 import axios from 'axios';
 
-//const res = axios.get()
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([46.227638, 2.213749], 6);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker1 = L.marker([51.5, -0.09]).addTo(map);
-var marker2 = L.marker([55.5, -9.09]).addTo(map);
-var marker3 = L.marker([43.30, -0.36]).addTo(map);
+//const socket = new WebSocket("ws://localhost:8080")
+axios.get("http://192.168.155.3:8000/get-gps-data")
+.then((res) => {
+    for (let index = 0; index < res.data.length; index++) {
+        L.marker([res.data[index].lat, res.data[index].lng]).addTo(map).bindPopup(res.data[index].name);
+        
+    }
+})
+.catch((error) => {
+    console.log(error)
+})
 
-marker1.bindPopup("GPS n°1")
-marker2.bindPopup("GPS n°2")
-marker3.bindPopup("GPS n°3")
+// socket.addEventListener("message", (event) => {
+//     console.log("Message from server ", event.data);
+//   });
